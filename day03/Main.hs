8e55da39663af60b6235a80ee7edf0de6f0a060c
Soeren Berg <soeren.berg@protonmail.com>
2022-12-11 20:14:31 +0100

    day03 part II

--- day03/Main.hs
+++ day03/Main.hs
@@ -2,6 +2,7 @@
 module Main (main) where
 
 import Data.Char (ord)
+import Data.Maybe (catMaybes)
 import qualified Data.Set as S
 
 priority :: Char -> Int
@@ -15,10 +16,22 @@ common (l, r) = S.toList $ sl `S.intersection` sr
   where sl = S.fromList l
         sr = S.fromList r
 
-rucksackPriority :: String -> Int
-rucksackPriority = sum . common . splitHalf . (map priority)
+rucksackPriority :: [Int] -> Int
+rucksackPriority = sum . common . splitHalf
+
+groupPriority :: ([Int], [Int], [Int]) -> Maybe Int
+groupPriority (a, b, c) = S.lookupMax cut
+  where cut = foldr1 S.intersection sets
+        sets = map S.fromList [a, b, c]
+
+threeTups :: [a] -> [(a, a, a)]
+threeTups (x:y:z:xs) = (x, y, z) : threeTups xs
+threeTups _ = []
 
 main :: IO ()
 main = do inputLines <- lines <$> readFile "data/day03.txt"
-          let partI = sum $ map rucksackPriority inputLines
-          putStrLn $ "total priorities (part I): " ++ show partI
+          let ps = map (map priority) inputLines
+          let partI = sum $ map rucksackPriority ps
+          let partII = sum $ catMaybes $ map groupPriority $ threeTups ps
+          putStrLn $ "total priorities (part I):        " ++ show partI
+          putStrLn $ "sum 3 group priorities (part II): " ++ show partII
