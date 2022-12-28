a85da79a5e7e2f9670e112e5122c01b852d86149
Soeren Berg <soeren.berg@protonmail.com>
2022-12-28 23:57:01 +0100

    day 17 part II

--- day17/Main.hs
+++ day17/Main.hs
@@ -9,14 +9,22 @@ import Lens.Micro.Platform ((%=), (<<%=), makeLenses , use)
 
 
 type Vec2 = (Int, Int)
+type Epoch = Int
+type Level = Int
 
-data S = S { _cave        :: S.Set Vec2  -- M.Map Vec2 Bool
-           , _rockIndex   :: Int
-           , _jet         :: M.Map Int Int
-           , _jetCount    :: Int
-           , _jetNum      :: Int
-           , _level       :: Int
-           , _epoch       :: Int
+data CacheState = CS { rockIdx    :: Int
+                     , jetIdx     :: Int
+                     , window     :: S.Set Vec2
+                     } deriving (Eq, Ord, Show)
+
+data S = S { _cave      :: S.Set Vec2
+           , _rockIndex :: Int
+           , _jet       :: M.Map Int Int
+           , _jetCount  :: Int
+           , _jetNum    :: Int
+           , _level     :: Int
+           , _epoch     :: Int
+           , _cache     :: M.Map CacheState (Epoch, Level)
            } deriving Show
 makeLenses ''S
 
@@ -28,18 +36,17 @@ initState s = S { _cave      = S.empty
                 , _jetNum    = length s
                 , _level     = 0
                 , _epoch     = 0
+                , _cache     = M.empty
                 }
   where xs = map (\c -> if c=='<' then (-1) else 1) s
 
-data Rock = Rock { _cells  :: [Vec2]
-                 , _kind   :: Int
-                 , _boundL :: Int
-                 , _boundR :: Int
-                 , _boundT :: Int
-                 , _boundB :: Int
+data Rock = Rock { cells  :: [Vec2]
+                 , kind   :: Int
+                 , boundL :: Int
+                 , boundR :: Int
+                 , boundT :: Int
+                 , boundB :: Int
                  } deriving (Show, Eq)
-makeLenses ''Rock
-
 
 popJet :: State S Int
 popJet = do n <- use jetNum
@@ -66,7 +73,7 @@ vshift z (Rock cs k l r t b) = Rock (map (\(x,y)->(x+z,y)) cs) k (l+z) (r+z) t b
 moveInitRock :: Rock -> State S Rock
 moveInitRock r =
   do (s1, s2, s3) <- pop3Jets
-     case (s1+s2, s1+s2+s3, _kind r) of
+     case (s1+s2, s1+s2+s3, kind r) of
        (-2, -1, _) -> pure $ vshift (-1) r
        (-2, -3, _) -> pure $ vshift (-2) r
        (0, -1,  _) -> pure $ vshift (-1) r
@@ -90,9 +97,9 @@ jetRock r = do r' <- flip vshift r <$> popJet
                pure $ if feas then r' else r
 
 fall :: Rock -> State S (Rock, Bool)
-fall r = do let r' = r { _cells = map (\(x,y)->(x,y-1)) $ _cells r
-                       , _boundT = _boundT r - 1  -- TODO lenses?
-                       , _boundB = _boundB r - 1}  -- TODO lenses?
+fall r = do let r' = r { cells = map (\(x,y)->(x,y-1)) $ cells r
+                       , boundT = boundT r - 1
+                       , boundB = boundB r - 1}
             feas <- isFeasible r'
             pure $ if feas then (r', True) else (r, False)
 
@@ -105,15 +112,42 @@ putRock (Rock cs _ _ _ t _) =
   do cave %= (\c -> foldr S.insert c cs)
      level %= (t `max`)
 
-simLevel :: State S Int
-simLevel =
-  do done <- (>=2022) <$> (epoch <<%= (+1))
+getCacheElt :: State S CacheState
+getCacheElt = do r <- use rockIndex
+                 j <- use jetCount
+                 l <- use level
+                 w <- S.filter ((>(l-64)) . snd) <$> use cave
+                 pure $ CS r j (S.map (\(x,y) -> (x, y-l)) w)
+
+updateCache :: State S (Maybe (Epoch, Level))
+updateCache = do ch <- getCacheElt
+                 lu <- (M.lookup ch) <$> use cache
+                 e <- use epoch
+                 l <- use level
+                 cache %= M.insert ch (e, l)
+                 pure lu
+
+useCycleToFinish :: Int -> (Epoch, Level) -> State S Int
+useCycleToFinish m (e, l) =
+  do currentEpoch <- use epoch
+     currentLevel <- use level
+     let cycleLength = currentEpoch - e
+     let cyclesRemaining = div (m - currentEpoch) cycleLength
+     remainder <- simLevel (m - cyclesRemaining * cycleLength + 1) False
+     pure $ cyclesRemaining * (currentLevel - l) + remainder
+
+simLevel :: Int -> Bool -> State S Int
+simLevel maxEpoch useCache =
+  do done <- (>=maxEpoch) <$> (epoch <<%= (+1))
      if done
        then use level
-       else do rc <- rockIndex <<%= (`mod` 5) . (+1)
-               r <- rocks rc <$> use level >>= moveInitRock >>= moveRock
-               putRock r
-               simLevel
+       else do cachedEpoch <- updateCache
+               case (useCache, cachedEpoch) of
+                 (True, Just t) -> useCycleToFinish maxEpoch t
+                 _ -> do rc <- rockIndex <<%= (`mod` 5) . (+1)
+                         r <- rocks rc <$> use level >>= moveInitRock >>= moveRock
+                         putRock r
+                         simLevel maxEpoch useCache
 
 -- Since we don't need to test collison during the first 3 moves (jet/fall), we
 -- spawn a rock 3 cells below than their actual position. We then save
@@ -127,5 +161,8 @@ rocks _ y = Rock [(3, y+2), (4, y+2), (3, y+1), (4, y+1)]           4 3 4 (y+2) 
 
 main :: IO ()
 main = do input <- ((!!0) . lines) <$> readFile "data/day17.txt"
-          let r = evalState simLevel (initState input)
-          putStrLn $ "part I : " ++ (show r)
+          let r = evalState (simLevel 2022 True) (initState input)
+          putStrLn $ "part I : " ++ (show $ r)
+
+          let r' = evalState (simLevel 1000000000000 True) (initState input)
+          putStrLn $ "part II: " ++ (show r')
